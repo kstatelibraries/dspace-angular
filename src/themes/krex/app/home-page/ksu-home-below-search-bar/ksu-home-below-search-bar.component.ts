028b2925b29ded3226adc77de737bdb42b34b636
Giridhar Katasani <giridhar@ksu.edu>
2026-03-27 14:25:27 -0500

    krex-theme-upgrade version 9.2
    - Fix: lint issues
    - es lint issues in ksu-header.component.ts
    - This is the commit message #4:
    - Lint errors
    - lint issues
    - removing trailing space
    
    Add: krex global theme styles
    - removal of auto generated unit test cases

---
+++ src/themes/krex/app/home-page/ksu-home-below-search-bar/ksu-home-below-search-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'ds-ksu-home-below-search-bar',
+  imports: [
+    RouterLink,
+  ],
+  templateUrl: './ksu-home-below-search-bar.component.html',
+  styleUrl: './ksu-home-below-search-bar.component.scss',
+})
+export class KsuHomeBelowSearchBarComponent {
+
+}
---
+++ src/themes/krex/app/ksu-environment-banner/ksu-environment-banner.component.ts
@@ -0,0 +1,32 @@
+import {
+  Component,
+  Inject,
+  OnInit,
+} from '@angular/core';
+import {
+  APP_CONFIG,
+  AppConfig,
+} from 'src/config/app-config.interface';
+
+@Component({
+  selector: 'ds-ksu-environment-banner',
+  imports: [],
+  templateUrl: './ksu-environment-banner.component.html',
+  styleUrl: './ksu-environment-banner.component.scss',
+})
+export class KsuEnvironmentBannerComponent implements OnInit {
+  bannerText = '';
+  bannerEnabled = false;
+  bannerStyle = {};
+
+  constructor(@Inject(APP_CONFIG) private appConfig: AppConfig) {
+  }
+
+  ngOnInit(): void {
+    const bannerConfig = this.appConfig['environmentBanner']; // eslint-disable-line @typescript-eslint/dot-notation
+
+    this.bannerText = bannerConfig?.text;
+    this.bannerEnabled = bannerConfig?.enabled;
+    this.bannerStyle = { 'color': bannerConfig?.foregroundColor, 'background-color': bannerConfig?.backgroundColor };
+  }
+}
